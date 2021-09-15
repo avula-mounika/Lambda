e6c1d88b8cd4f4ee6a3c307e95d5f7c27fdea2af
avula-mounika <goudmounika13@gmail.com>
2021-09-15 19:20:02 +0530

    lambda function

---
+++ LambdaFunction.java
@@ -0,0 +1,26 @@
+
+interface Arithmetic {
+	int operation(int a, int b);
+}
+
+public class LambdaFunction {
+	public static void main(String[] args) {
+
+		// Addition using Lambda expression
+		Arithmetic addition = (int a, int b) -> (a + b);
+		System.out.println("Addition = " + addition.operation(5, 6));
+
+		// Subtraction using Lambda expression
+		Arithmetic subtraction = (int a, int b) -> (a - b);
+		System.out.println("Subtraction = " + subtraction.operation(5, 3));
+
+		// Multiplication using Lambda expression
+		Arithmetic multiplication = (int a, int b) -> (a * b);
+		System.out.println("Multiplication = " + multiplication.operation(4, 6));
+
+		// Division using Lambda expression
+		Arithmetic division = (int a, int b) -> (a / b);
+		System.out.println("Division = " + division.operation(12, 6));
+
+	}
+}
